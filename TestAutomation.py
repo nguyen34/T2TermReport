8da406315d5c80b55817388a406466cfe2a657f1
nguyen34 <nguyen_quoc_huy@hotmail.ca>
2015-09-02 14:04:58 -0700

    Updated TestAutomation.py to create logs for test results

--- TestAutomation.py
+++ TestAutomation.py
@@ -5,18 +5,24 @@ import time
 inputPort = "ipc://input"
 # Specift the ipc/tcp port that the business logic will be publishing output on
 outputPort = "ipc://output"
+# Set up global counter variables simply to count the number of tests run and failed tests.
+testCounter = 0
+failCounter = 0
 # Specify Test Cases here under main()
 
 def main():
-    
-   result = calculate("123" , 13, '+')
-   print(result)
-   result = calculate("123" , 13, '-')
-   print(result)
-   result = calculate("100", 10, '/')
-   print(result)
-   result = calculate("123" , 13, '*')
-   print(result)
+   log = open("log.txt", "w")
+   print("Test Suite Started.")
+   #specify test cases here, using test method with the following parameters: [integer1], [integer2], [operation], [expected]
+   #to test the following equation: [integer1] [operation] [integer2] = [expected]
+   ####################################################################################################################################
+   #Test cases:
+   test(1, 2,"+", 3)
+   test(2, 2, "-", 0)
+   test(1, 4, "+", 6)
+
+
+   
 
 
 
@@ -25,15 +31,46 @@ def main():
 
 
 
+   ####################################################################################################################################
+   print("Test Suite Finished.")
+   print(str(testCounter) + " tests run.")
 
+   if failCounter < 1:
+      print("All Passed.")
+   else:
+      print(str(failCounter) + " failed. Please see log for more details.")
+   log.close()
+
+
+
+
+def test(int1, int2, op, expected):
+   try:
+      e = int(expected)
+   except ValueError:
+      print("integer1 is not an integer.")
+      return False
+   global log
+   global testCounter
+
+   if testCounter == 0:
+      log = open("log.txt", "w")
+   else:
+      log = open("log.txt", "a")
+   
+   testCounter += 1
+   result = calculate(int1, int2, op)
+   if int(result) == e:
+      log.write("Test " + str(testCounter) + ": Passed!\n")
+   else:
+      global failCounter
+      failCounter += 1
+      log.write("Test " + str(testCounter) + ": Failed! Expected: " + str(expected) + " Actual: " + str(result) + "\n")
+   log.close()
+      
 
 # calculate method accepts three parameters, an [integer1], an [integer2] and an [operation] sign
 # and construct an equation in the following format: [integer 1] [operation sign] [integer 2] (e.g. 1234 + 12)
-
-
-    
-
-
 
 def calculate (integer1, integer2, operation):
     try:
