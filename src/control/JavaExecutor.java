4c48075ce676481ead085a84040e93554a79500e
nguyen34 <nguyen_quoc_huy@hotmail.ca>
2015-09-08 20:27:58 -0700

    Changed port addresses for java program, added extra line to JavaExecutor class so now it can manually close processes with a command

--- src/control/JavaExecutor.java
+++ src/control/JavaExecutor.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class JavaExecutor {
+	
+	public static final int MAX_LEN = 255;
+	
 	public static void main(String[] args) throws Exception{
 		 
 		
@@ -11,22 +14,31 @@ public class JavaExecutor {
 		Process q = Runtime.getRuntime().exec(new String[]{"bash", "-c", "export LD_LIBRARY_PATH=/usr/local/lib/ && export JAVA_LIBRARY_PATH=/usr/local/lib/ && cd /home/johnson/git/T2TermReport/src/ && java -cp /usr/local/share/java/zmq.jar:/usr/local/lib:. businessLogic/BusinessLogic"});
 		Process r = Runtime.getRuntime().exec(new String[]{"bash", "-c", "export LD_LIBRARY_PATH=/usr/local/lib/ && export JAVA_LIBRARY_PATH=/usr/local/lib/ && cd /home/johnson/git/T2TermReport/src/ && java -cp /usr/local/share/java/zmq.jar:/usr/local/lib:. output/OutputUI"});
 		
-		while (!Thread.currentThread().isInterrupted()) {
-		BufferedReader pReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line = "";
-		while ((line = pReader.readLine()) != null){
-			System.out.println(line);	
+		
+		
+		
+		String command = "";
+		int len = 1;
+		while (!command.equals("kill")){
+	
+		System.out.println("Java Calculator Components Executed. To terminate processes, type in 'kill' and press [Enter]");
+		System.out.print("> ");
+		byte cmd[] = new byte[MAX_LEN];
+		len = System.in.read(cmd);
+		
+		if (len <= 0)
+			break;
+		command = new String(cmd);
+		command = command.trim();
+	    command = command.toLowerCase();
+		if (command.equals("kill")){
+			p.destroy();
+			q.destroy();
+			r.destroy();
+		} else{
+			System.out.println("Invalid Command");
 		}
 		
-		BufferedReader qReader = new BufferedReader(new InputStreamReader(q.getInputStream()));
-		line = "";
-		while ((line = qReader.readLine()) != null){
-			System.out.println(line);
-		}
-		BufferedReader rReader = new BufferedReader(new InputStreamReader(r.getInputStream()));
-		line = "";
-		while ((line = rReader.readLine()) != null){
-			System.out.println(line);
 		}
 		
 		
@@ -39,4 +51,4 @@ public class JavaExecutor {
 	
 	
 	
-}
+
